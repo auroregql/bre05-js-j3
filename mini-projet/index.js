e143b3757d08ff4cb8ee285ccb4f5d8ad3e71b85
auroregql <aurore.gicquel.06@gmail.com>
2026-01-08 13:23:00 +0000

    Mini-projet terminé : Changer la couleur de fond des divs

--- mini-projet/index.js
+++ mini-projet/index.js
@@ -11,14 +11,15 @@ noir: #020e13;
 
 */
 
-function grid1()
-{
-    let section1 = document.querySelectorAll("main section")[0];
+function grid1() {
     
-    let divs = document.querySelectorAll("div");
+    let section1 = document.querySelectorAll("main section")[0];
 
-    for (let i = 0; i < divs.length; i++) 
-    {
+    
+    let divs = section1.querySelectorAll("div");
+
+    
+    for (let i = 0; i < divs.length; i++) {
         divs[i].style.backgroundColor = "#020e13";
     }
 }
@@ -49,21 +50,70 @@ function grid3()
 
     
      let divs = section3.querySelectorAll("div");
+     
+     divs[0].style.backgroundColor = "#e85b69";
+     divs[1].style.backgroundColor = "#eca45c";
+     divs[2].style.backgroundColor = "#efec4f";
+     divs[3].style.backgroundColor = "#4cee7e";
+     divs[4].style.backgroundColor = "#427cf5";
+     divs[5].style.backgroundColor = "#956caf";
 }
 
 function grid4()
 {
+    let section4 = document.querySelectorAll("main section")[3];
 
+    
+    let divs = section4.querySelectorAll("div");
+
+    
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].style.backgroundColor = "#020e13";
+    }
+    
+    divs[4].style.backgroundColor = "#4cee7e";
 }
 
 function grid5()
 {
+    let section5 = document.querySelectorAll("main section")[4];
 
+    
+    let divs = section5.querySelectorAll("div");
+
+    
+    for (let i = 0; i <= 2; i++) {
+        divs[i].style.backgroundColor = "#427cf5";
+        
+    }
+        
+    for (let i = 3; i <= 5; i++){
+        divs[i].style.backgroundColor = "#956caf";
+    }
+    
+    for (let i = 6; i <= 8; i++) {
+        divs[i].style.backgroundColor = "#eca45c";
+    }
+    
+    divs[4].style.backgroundColor = "#e85b69";
+    divs[7].style.backgroundColor = "#efec4f";
 }
 
-function grid6()
-{
+function grid6() {
+    let section6 = document.querySelectorAll("main section")[5];
+    let divs = section6.querySelectorAll("div");
 
+    for (let i = 0; i <= 2; i++) {
+        divs[i].style.background = "linear-gradient(to bottom, #956caf, #427cf5)";
+    }
+    
+    for (let i = 3; i <= 5; i++){
+        divs[i].style.background = "linear-gradient(to bottom, #4cee7e, #efec4f)";
+    }
+    
+    for (let i = 6; i <= 8; i++) {
+        divs[i].style.background = "linear-gradient(to bottom, #eca45c, #e85b69)";
+    }
 }
 
 grid1();
